f02f455d720d80d7536f2af22b28bb045c6c1389
riyasaheblal <riyagupta7532@gmail.com>
2025-06-16 12:10:50 +0530

    Initial commit

---
+++ src/main/java/com/cedge/rg/Java8Features/pract/Pract1.java
@@ -0,0 +1,7 @@
+package com.cedge.rg.Java8Features.pract;
+
+public class Pract1 {
+public static void main(String[] args) {
+	System.out.println("Hello Java");
+}
+}
---
+++ src/test/java/com/cedge/rg/Java8Features/Java8FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedge.rg.Java8Features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
